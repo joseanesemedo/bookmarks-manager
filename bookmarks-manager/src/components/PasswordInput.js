757af41b62832b573d24c0d4e7c6b9627fd56a42
Joseane Semedo <joseanelmsemedo2@gmail.com>
2023-03-02 14:53:41 +0000

    Changes on PasswordInput

--- bookmarks-manager/src/components/PasswordInput.js
+++ bookmarks-manager/src/components/PasswordInput.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import "./PasswordInput.scss";
 
-const PasswordInput = () => {
-  return <div>PasswordInput</div>;
+const PasswordInput = ({ onChange, value }) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
+
+  return (
+    <div className="password__input">
+      <input
+        type={showPassword ? "text" : "password"}
+        placeholder="Password"
+        value={value}
+        onChange={onChange}
+      />
+      <button
+        className="btn__icon"
+        type="button"
+        onClick={handleClickShowPassword}
+      >
+        {showPassword ? (
+          <i className="icon ri-eye-off-line"></i>
+        ) : (
+          <i className="icon ri-eye-line"></i>
+        )}
+      </button>
+    </div>
+  );
 };
 
 export default PasswordInput;
--- bookmarks-manager/src/pages/SignIn.js
+++ bookmarks-manager/src/pages/SignIn.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import PasswordInput from "../components/PasswordInput";
+import { Link } from "react-router-dom";
+
+import "./Form.scss";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  return (
+    <div>
+      <h1>Sign in</h1>
+      <form action="">
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <PasswordInput
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <button className="">Sign in</button>
+      </form>
+
+      {errorMessage ? <span className="">{errorMessage}</span> : null}
+      <span>
+        Don't have an account? <Link to={"signup"}>Create an account</Link>
+      </span>
+    </div>
+  );
 };
 
 export default SignIn;
