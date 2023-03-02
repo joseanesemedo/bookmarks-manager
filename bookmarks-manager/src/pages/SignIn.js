9a9a5d2807afade9f064e9e0078aa9ca81111c76
Joseane Semedo <joseanelmsemedo2@gmail.com>
2023-03-02 15:04:00 +0000

    added validateForm function to SignIn page

--- bookmarks-manager/src/pages/SignIn.js
+++ bookmarks-manager/src/pages/SignIn.js
@@ -7,22 +7,43 @@ import "./Form.scss";
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
   const [errorMessage, setErrorMessage] = useState("");
+
+  function handleLogin(e) {
+    e.preventDefault();
+    validateForm();
+
+    console.log(email, password);
+  }
+
+  const validateForm = () => {
+    let validation = {};
+    validation.email = email ? "" : "Este campo é obrigatório";
+    validation.password = password ? "" : "Este campo é obrigatório";
+
+    setErrors({
+      ...validation,
+    });
+  };
 
   return (
     <div>
       <h1>Sign in</h1>
-      <form action="">
+      <form onSubmit={handleLogin}>
         <input
           type="text"
           placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
+        <h3>{errors.email && errors.email}</h3>
         <PasswordInput
           onChange={(e) => setPassword(e.target.value)}
           value={password}
         />
+        <h3>{errors.password && errors.password}</h3>
+
         <button className="">Sign in</button>
       </form>
 
