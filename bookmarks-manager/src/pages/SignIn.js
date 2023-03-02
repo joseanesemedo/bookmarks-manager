a6585772a5b27210d687325044c1b3fbc52933a8
Joseane Semedo <joseanelmsemedo2@gmail.com>
2023-03-02 15:14:01 +0000

    added assets folder

--- bookmarks-manager/src/pages/SignIn.js
+++ bookmarks-manager/src/pages/SignIn.js
@@ -28,29 +28,29 @@ const SignIn = () => {
   };
 
   return (
-    <div>
-      <h1>Sign in</h1>
-      <form onSubmit={handleLogin}>
-        <input
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <h3>{errors.email && errors.email}</h3>
-        <PasswordInput
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-        <h3>{errors.password && errors.password}</h3>
-
-        <button className="">Sign in</button>
-      </form>
-
-      {errorMessage ? <span className="">{errorMessage}</span> : null}
-      <span>
-        Don't have an account? <Link to={"signup"}>Create an account</Link>
-      </span>
+    <div className="form__container">
+      <div className="form__container__wrapper">
+        <h1>Sign in</h1>
+        <form onSubmit={handleLogin}>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h3>{errors.email && errors.email}</h3>
+          <PasswordInput
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <h3>{errors.password && errors.password}</h3>
+          <button className="">Sign in</button>
+        </form>
+        {errorMessage ? <span className="">{errorMessage}</span> : null}
+        <span>
+          Don't have an account? <Link to={"signup"}>Create an account</Link>
+        </span>
+      </div>
     </div>
   );
 };
