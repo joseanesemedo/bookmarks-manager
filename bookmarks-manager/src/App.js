b799833bce9dad14976fc32e412ddf04b3667b25
Joseane Semedo <joseanelmsemedo2@gmail.com>
2023-03-02 14:23:29 +0000

    added dark mode

--- bookmarks-manager/src/App.js
+++ bookmarks-manager/src/App.js
@@ -1,23 +1,33 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { DarkModeSwitch } from "react-toggle-dark-mode";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${theme}`}>
+      {/* <button onClick={toggleTheme}>Toggle Theme</button> */}
+
+      <DarkModeSwitch
+        // style={{ height: "40px" }}
+        checked={theme}
+        onClick={toggleTheme}
+        size={30}
+        sunColor={"#eab308"}
+      />
+      <h1>Hello, world!</h1>
     </div>
   );
 }
-
 export default App;
