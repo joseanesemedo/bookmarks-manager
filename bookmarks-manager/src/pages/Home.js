6a1c0a53f5b60473b9ccbffd4ff9a2cfe51ab996
Joseane Semedo <joseanelmsemedo2@gmail.com>
2023-03-02 19:18:46 +0000

    added handleAddTag function

--- bookmarks-manager/src/pages/Home.js
+++ bookmarks-manager/src/pages/Home.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.scss";
+import supabase from "../supabase";
 
 const Home = ({ session }) => {
+  const [showForm, setShowForm] = useState(false);
+  const [tagName, setTagName] = useState("");
+
+  async function handleAddTag(e) {
+    e.preventDefault();
+    console.log(tagName);
+
+    const { data: newTag, error } = await supabase
+      .from("tags")
+      .insert([{ name: tagName, uid: session.user.id }])
+      .select();
+  }
+
   return (
     <main className="main">
       <aside>
@@ -10,11 +24,29 @@ const Home = ({ session }) => {
             <button className="btn btn__tags">All</button>
           </li>
           <li className="category">
-            <button className="btn btn__add__tag">Add tag +</button>
+            <button
+              className="btn btn__add__tag"
+              onClick={() => setShowForm((show) => !show)}
+            >
+              Add tag +
+            </button>
           </li>
-          <li className="category">
-            <button className="btn btn__tag">All</button>
-          </li>
+
+          {showForm && (
+            <div className="tag__form__container">
+              <form className="tag__form">
+                <input
+                  type="text"
+                  value={tagName}
+                  onChange={(e) => {
+                    setTagName(e.target.value);
+                  }}
+                />
+                <button onClick={handleAddTag}>add</button>
+              </form>
+            </div>
+          )}
+
           <li className="category">
             <button className="btn btn__tag">All</button>
           </li>
